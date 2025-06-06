c74c14efd047237fa850b6aa5e65265fd24c7f5e
Tarun Raja <tarun@Taruns-MacBook-Air.local>
2025-06-06 18:49:31 +1000

    initialisation

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,6 +2,8 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
+  console.log(block);
+
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
